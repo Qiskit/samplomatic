012684f063b5e95a3ac545de25da501ec4f733eb
saanaz379 <79679653+saanaz379@users.noreply.github.com>
2025-08-08 14:02:34 -0400

    migrate

--- samplomatic/transpiler/passes/__init__.py
+++ samplomatic/transpiler/passes/__init__.py
@@ -16,3 +16,4 @@ from .add_inject_noise import AddInjectNoise
 from .add_terminal_right_dressed_boxes import AddTerminalRightDressedBoxes
 from .group_gates_into_boxes import GroupGatesIntoBoxes
 from .group_meas_into_boxes import GroupMeasIntoBoxes
+from .merge_boxes import MergeBoxes
