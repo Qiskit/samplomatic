0d6d6f0e61112116a27999296be74c4d5b63d36d
SamFerracin <sam.ferracin@ibm.com>
2025-08-26 14:26:54 -0400

    removed unnecessary tests

--- test/integration/test_generate_boxing_pass_manager.py
+++ test/integration/test_generate_boxing_pass_manager.py
@@ -13,79 +13,34 @@
 """Test that `generate_boxing_pass_manager` generates buildable circuits."""
 
 import pytest
-from qiskit.circuit import Parameter, QuantumCircuit, QuantumRegister
+from qiskit.circuit import ClassicalRegister, Parameter, QuantumCircuit, QuantumRegister
 
 from samplomatic import build
 from samplomatic.transpiler import generate_boxing_pass_manager
 
 
 def make_circuits():
-    theta = Parameter("theta")
-    phi = Parameter("phi")
-    lam = Parameter("lambda")
-
     circuit = QuantumCircuit(1)
 
     yield circuit, "empty_circuit"
 
-    qregs = [QuantumRegister(4, "alpha"), QuantumRegister(2, "beta")]
-    circuit = QuantumCircuit(*qregs)
-    circuit.cx(1, 2)
-    circuit.cx(4, 3)
-    circuit.x(0)
-    circuit.rx(theta, 0)
-    circuit.z(1)
-
-    yield circuit, "multiple_quantum_registers"
-
-    circuit = QuantumCircuit(6)
-    circuit.cx(0, 1)
-    circuit.cx(1, 2)
-    circuit.cx(2, 3)
-    circuit.cx(3, 4)
-    circuit.cx(4, 5)
-    circuit.cx(4, 3)
-
-    yield circuit, "each_gate_in_a_separate_box"
-
-    circuit = QuantumCircuit(6)
-    circuit.x(0)
-    circuit.rx(theta, 0)
-    circuit.rz(phi, 1)
-    circuit.z(0)
-    circuit.y(1)
-    circuit.cx(1, 2)
-    circuit.cx(4, 3)
-    circuit.ecr(0, 1)
-    circuit.y(1)
-    circuit.sx(3)
-    circuit.rz(lam, 5)
-    circuit.sx(5)
-
-    yield circuit, "each_box_contains_single_and_multi-qubit_gates"
-
-    circuit = QuantumCircuit(4)
-    circuit.cx(0, 1)
-    circuit.x(0)
-    circuit.z(1)
-    circuit.y(2)
-    circuit.h(3)
-    circuit.barrier(1, 2)
-    circuit.cx(2, 3)
-
-    yield circuit, "circuit_with_partial_width_barrier"
-
     circuit = QuantumCircuit(3)
     circuit.cx(0, 1)
     circuit.x(0)
-    circuit.y(0)
-    circuit.t(1)
-    circuit.barrier()
+    circuit.x(1)
+    circuit.barrier(1)
     circuit.z(1)
-    circuit.h(2)
-    circuit.y(2)
+    circuit.x(2)
+    circuit.cx(0, 1)
+    circuit.x(1)
+    circuit.barrier(0, 1)
+    circuit.cx(0, 1)
+    circuit.cx(0, 2)
+    circuit.barrier(0, 1, 2)
+    circuit.x(0)
+    circuit.x(1)
 
-    yield circuit, "circuit_with_full_width_barrier"
+    yield circuit, "circuit_with_barriers"
 
     circuit = QuantumCircuit(3)
     circuit.cx(0, 1)
@@ -106,7 +61,7 @@ def make_circuits():
     circuit.x(0)
     circuit.x(1)
 
-    yield circuit, "circuit_with_partial_width_and_full_width_boxes"
+    yield circuit, "circuit_with_boxes"
 
     circuit = QuantumCircuit(4, 4)
     circuit.measure(1, 0)
@@ -134,7 +89,7 @@ def make_circuits():
 
     yield circuit, "circuit_with_barriers_and_measurements"
 
-    circuit = QuantumCircuit(6, 6)
+    circuit = QuantumCircuit(QuantumRegister(6, "q"), ClassicalRegister(6, "c"))
     for layer_idx in range(8):
         for qubit_idx in range(circuit.num_qubits):
             circuit.rz(Parameter(f"theta_{layer_idx}_{qubit_idx}"), qubit_idx)
@@ -165,8 +120,9 @@ def pytest_generate_tests(metafunc):
     "inject_noise_strategy",
     ["none", "no_modification", "uniform_modification", "individual_modification"],
 )
+@pytest.mark.parametrize("remove_barriers", [True, False])
 def test_generate_boxing_pass_manager_makes_buildable_circuits(
-    circuit, enable_gates, enable_measure, twirling_strategy, inject_noise_strategy
+    circuit, enable_gates, enable_measure, twirling_strategy, inject_noise_strategy, remove_barriers
 ):
     """Test `generate_boxing_pass_manager`.
 
@@ -174,10 +130,11 @@ def test_generate_boxing_pass_manager_makes_buildable_circuits(
         circuit: The circuit to try and build
     """
     pm = generate_boxing_pass_manager(
-        enable_gates,
-        enable_measure,
-        twirling_strategy,
-        inject_noise_strategy,
+        enable_gates=enable_gates,
+        enable_measure=enable_measure,
+        twirling_strategy=twirling_strategy,
+        inject_noise_strategy=inject_noise_strategy,
+        remove_barriers=remove_barriers,
     )
     transpiled_circuit = pm.run(circuit)
 
