Qiskit/samplomatic

commit 7be30ac395e05a8dda812b22a410dd940212a55b

Tensorize noise models (#112)
## Summary

Closes #103.

## Details and comments

samplomatic/samplex/noise_model_requirement.py
@@ -1,0 +1,51 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2025.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""NoiseModelRequirement"""
+
+from dataclasses import dataclass, field
+from typing import Any
+
+from qiskit.quantum_info import QubitSparsePauliList
+
+
+@dataclass
+class NoiseModelRequirement:
+    """A class that represents a noise model required for sampling."""
+
+    noise_ref: str
+    """A unique reference to this handle."""
+
+    num_qubits: int
+    """The number of qubits this model acts on."""
+
+    noise_modifiers: set = field(default=set)
+    """The set of modifiers that act on this noise model."""
+
+    def _to_json_dict(self) -> dict[str, str]:
+        return {
+            "noise_ref": self.noise_ref,
+            "num_qubits": self.num_qubits,
+            "noise_modifiers": [modifier for modifier in self.noise_modifiers],
+        }
+
+    @classmethod
+    def _from_json(cls, data: dict[str, Any]) -> "NoiseModelRequirement":
+        data["noise_modifiers"] = set(data["noise_modifiers"])
+        return cls(**data)
+
+    def validate_noise_model(self, value: QubitSparsePauliList):
+        if self.num_qubits != value.num_qubits:
+            raise ValueError(
+                f"Noise model for '{self.noise_ref}' is expected to act on '{self.num_qubits}` "
+                f"systems, not '{value.num_qubits}'."
+            )

samplomatic/samplex/samplex.py
@@ -12,11 +12,13 @@
 
 """Samplex"""
 
-from collections.abc import Iterable, Sequence
+from collections.abc import Iterable, Mapping, Sequence
 from concurrent.futures import Future, ThreadPoolExecutor, as_completed, wait
 from typing import TYPE_CHECKING, Self
 
+import numpy as np
 from numpy.random import Generator, SeedSequence, default_rng
+from qiskit.quantum_info import QubitSparsePauliList
 from rustworkx.rustworkx import PyDiGraph, topological_generations
 
 from ..aliases import (
@@ -34,11 +36,12 @@ from ..aliases import (
 )
 from ..annotations import VirtualType
 from ..exceptions import SamplexConstructionError, SamplexRuntimeError
-from ..tensor_interface import Specification, TensorInterface, TensorSpecification
+from ..tensor_interface import Specification, TensorInterface, TensorSpecification, ValueType
 from ..virtual_registers import VirtualRegister
 from ..visualization import plot_graph
 from .interfaces import SamplexOutput
 from .nodes import CollectionNode, EvaluationNode, Node, SamplingNode
+from .noise_model_requirement import NoiseModelRequirement
 from .parameter_expression_table import ParameterExpressionTable
 
 if TYPE_CHECKING:
@@ -67,6 +70,7 @@ class Samplex:
         self._evaluation_streams: list[list[EvaluationNode]] = []
         self._sampling_nodes: list[SamplingNode] = []
         self._collection_nodes: list[CollectionNode] = []
+        self._noise_model_requirements: dict[str, NoiseModelRequirement] = {}
         self._input_specifications: dict[InterfaceName, Specification] = {}
         self._output_specifications: dict[InterfaceName, Specification] = {}
 
@@ -83,6 +87,11 @@ class Samplex:
     def parameters(self) -> list[Parameter]:
         """The sorted parameters expecting values at sampling time."""
         return self._param_table.parameters
+
+    @property
+    def noise_model_requirements(self) -> dict[str, NoiseModelRequirement]:
+        """The noise models required at sampling time."""
+        return self._noise_model_requirements
 
     @property
     def num_parameters(self) -> int:
@@ -150,6 +159,18 @@ class Samplex:
         if (name := specification.name) in self._output_specifications:
             raise SamplexConstructionError(f"An output with name '{name}' already exists.")
         self._output_specifications[name] = specification
+
+    def add_noise_model_requirement(self, noise_model: NoiseModelRequirement):
+        """Add a noise model requirement to this samplex.
+
+        Args:
+            noise_model: The requirement to add.
+        """
+        if (noise_ref := noise_model.noise_ref) in self._noise_model_requirements:
+            raise SamplexConstructionError(
+                f"A noise model requirement with reference '{noise_ref}' already exists."
+            )
+        self._noise_model_requirements[noise_ref] = noise_model
 
     def add_node(self, node: Node) -> NodeIndex:
         """Add a node to the samplex graph.
@@ -229,13 +250,77 @@ class Samplex:
 
         return self
 
-    def inputs(self) -> TensorInterface:
+    def inputs(
+        self, noise_model_paulis: Mapping[str, QubitSparsePauliList] | None = None
+    ) -> TensorInterface:
         """Return an object that specifies and helps build the required inputs of :meth:`~sample`.
+
+        Args:
+            noise_model_paulis: The Pauli terms contained in the noise models. Each element of
+                :meth:`~noise_model_requirements` should be specified.
+
+        Raises:
+            ValueError: If ``noise_model_paulis`` has different keys than
+                :meth:`~noise_model_requirements`.
+            ValueError: If any of the ``noise_model_paulis`` has a different number of qubits than
+                its requirement.
 
         Returns:
             The input for this samplex.
         """
-        return TensorInterface(self._input_specifications.values())
+        noise_model_paulis = {} if noise_model_paulis is None else noise_model_paulis
+        if noise_model_paulis.keys() != self._noise_model_requirements.keys():
+            required_paulis = "\n".join(
+                f" * {ref}: '{req.num_qubits}' qubits."
+                for ref, req in self._noise_model_requirements.items()
+            )
+            raise ValueError(
+                f"The samplex input requires the following noise models:\n{required_paulis}"
+            )
+
+        specs = [*self._input_specifications.values()]
+        for name, noise_req in self._noise_model_requirements.items():
+            noise_req.validate_noise_model(paulis := noise_model_paulis[name])
+            specs.append(
+                Specification(
+                    f"noise_maps.paulis.{name}",
+                    ValueType.PAULIS,
+                    "The Pauli operators present in a noise map.",
+                )
+            )
+            specs.append(
+                TensorSpecification(
+                    f"noise_maps.rates.{name}",
+                    (num_terms := (paulis.num_terms),),
+                    np.dtype(np.float64),
+                    f"The rates of a noise map with ``{num_terms}`` terms acting on "
+                    f"``{noise_req.num_qubits}`` qubits.",
+                )
+            )
+
+            for noise_modifier in noise_req.noise_modifiers:
+                specs.append(
+                    TensorSpecification(
+                        f"noise_scales.{noise_modifier}",
+                        (),
+                        np.dtype(np.float64),
+                        "A factor by which to scale a noise map.",
+                        optional=True,
+                    )
+                )
+                specs.append(
+                    TensorSpecification(
+                        f"local_scales.{noise_modifier}",
+                        (num_terms,),
+                        np.dtype(np.float64),
+                        "An array of factors by which to scale individual rates of a noise map.",
+                        optional=True,
+                    )
+                )
+
+        return TensorInterface(specs).bind(
+            **{f"noise_maps.paulis.{name}": paulis for name, paulis in noise_model_paulis.items()}
+        )
 
     def outputs(self, num_randomizations: int) -> SamplexOutput:
         """Returns an object that specifies the promised outputs of :meth:`~sample`.

samplomatic/pre_samplex/pre_samplex.py
@@ -74,8 +74,9 @@ from ..samplex.nodes.pauli_past_clifford_node import (
     PAULI_PAST_CLIFFORD_INVARIANTS,
     PAULI_PAST_CLIFFORD_LOOKUP_TABLES,
 )
+from ..samplex.noise_model_requirement import NoiseModelRequirement
 from ..synths import Synth
-from ..tensor_interface import Specification, TensorSpecification, ValueType
+from ..tensor_interface import TensorSpecification
 from ..virtual_registers import U2Register
 from ..visualization import plot_graph
 from .graph_data import (
@@ -177,7 +178,7 @@ class PreSamplex:
         cregs: list[ClassicalRegister] | None = None,
         noise_map_count: count | None = None,
         noise_maps: dict[str, NumSubsystems] | None = None,
-        noise_modifiers: set[str] | None = None,
+        noise_modifiers: dict[str, set[str]] | None = None,
         basis_transforms: dict[str, int] | None = None,
         twirled_clbits: set[ClbitIndex] | None = None,
         passthrough_params: ParamSpec | None = None,
@@ -194,7 +195,7 @@ class PreSamplex:
         self._cregs = cregs
         self._noise_map_count = count() if noise_map_count is None else noise_map_count
         self._noise_maps = {} if noise_maps is None else noise_maps
-        self._noise_modifiers = set() if noise_modifiers is None else noise_modifiers
+        self._noise_modifiers = defaultdict(set) if noise_modifiers is None else noise_modifiers
         self._basis_transforms = {} if basis_transforms is None else basis_transforms
         self._twirled_clbits = set() if twirled_clbits is None else twirled_clbits
         self.passthrough_params: ParamSpec = (
@@ -621,7 +622,9 @@ class PreSamplex:
             )
         else:
             self._noise_maps[noise_ref] = len(qubits)
-        self._noise_modifiers.add(modifier_ref)
+
+        if modifier_ref:
+            self._noise_modifiers[noise_ref].add(modifier_ref)
 
         subsystems = self.qubits_to_indices(qubits)
         node = PreInjectNoise(
@@ -658,7 +661,9 @@ class PreSamplex:
             )
         else:
             self._noise_maps[noise_ref] = len(qubits)
-        self._noise_modifiers.add(modifier_ref)
+
+        if modifier_ref:
+            self._noise_modifiers[noise_ref].add(modifier_ref)
 
         subsystems = self.qubits_to_indices(qubits)
         node = PreInjectNoise(
@@ -1086,35 +1091,9 @@ class PreSamplex:
                 )
             )
 
-        for noise_map, length in self._noise_maps.items():
-            samplex.add_input(
-                Specification(
-                    f"noise_maps.{noise_map}",
-                    ValueType.LINDBLAD,
-                    f"A noise map acting on ``{length}`` qubits.",
-                )
-            )
-
-        for noise_modifier in self._noise_modifiers:
-            if noise_modifier == "":
-                continue
-
-            samplex.add_input(
-                TensorSpecification(
-                    f"noise_scales.{noise_modifier}",
-                    (),
-                    np.dtype(np.float64),
-                    "A factor by which to scale a noise map.",
-                    optional=True,
-                )
-            )
-            samplex.add_input(
-                Specification(
-                    f"local_scales.{noise_modifier}",
-                    ValueType.NUMPY_ARRAY,
-                    "An array of factors by which to scale individual elements of a noise map.",
-                    True,
-                )
+        for noise_ref, num_qubits in self._noise_maps.items():
+            samplex.add_noise_model_requirement(
+                NoiseModelRequirement(noise_ref, num_qubits, self._noise_modifiers[noise_ref])
             )
 
         if max_param_idx is not None:
@@ -1212,7 +1191,7 @@ class PreSamplex:
         node = InjectNoiseNode(
             reg_name,
             sign_reg_name,
-            "noise_maps." + pre_inject.ref,
+            pre_inject.ref,
             len(pre_inject.subsystems),
             pre_inject.modifier_ref,
         )

samplomatic/samplex/nodes/inject_noise_node.py
@@ -17,7 +17,6 @@ from qiskit.quantum_info import PauliLindbladMap
 
 from ...aliases import NumSubsystems, RegisterName, StrRef
 from ...annotations import VirtualType
-from ...exceptions import SamplexRuntimeError
 from ...virtual_registers import PauliRegister, Z2Register
 from .sampling_node import SamplingNode
 
@@ -26,14 +25,13 @@ class InjectNoiseNode(SamplingNode):
     """A node that produces samples for injecting noise.
 
     The node uses a :class:`qiskit.quantum_info.PauliLindbladMap` as the distribution
-    to draw samples from. This map can be modified at sample time with the following keyword
-    arguments, applied in order:
+    to draw samples from. This map can be modified at sample time:
 
     * ``noise_scales``, a dictionary from :class:`~.StrRef` to floats. If ``modifier_ref``
       is in the dictionary, the distribution is replaced with the one generated by
       by :meth:`qiskit.quantum_info.PauliLindbladMap.scale_rates`.
-    * ``local_scales``, a dictionary from :class:`~.StrRef` to lists of floats. The list should be
-      the same length as the map used. If ``modifier_ref`` is in the dictionary, the rates of
+    * ``local_scales``, a dictionary from :class:`~.StrRef` to lists of floats. If ``modifier_ref``
+      is in the dictionary, the rates of
       :class:`qiskit.quantum_info.PauliLindbladMap` are scaled individually.
 
     Args:
@@ -89,35 +87,18 @@ class InjectNoiseNode(SamplingNode):
         }
 
     def sample(self, registers, rng, inputs, num_randomizations):
-        if (noise_map := inputs.get(self._noise_ref)) is None:
-            raise SamplexRuntimeError(f"A noise map for '{self._noise_ref}' was not specified.")
-        if (num_qubits := noise_map.num_qubits) != self._num_subsystems:
-            raise SamplexRuntimeError(
-                f"Received a noise map acting on `{num_qubits}` qubits for "
-                f"'{self._noise_ref}' when it requires `{self._num_subsystems}`."
-            )
+        rates = inputs.get(f"noise_maps.rates.{self._noise_ref}")
+        paulis = inputs.get(f"noise_maps.paulis.{self._noise_ref}")
         if self._modifier_ref:
             scale = inputs.get("noise_scales." + self._modifier_ref, 1.0)
             local_scale = inputs.get(
-                "local_scales." + self._modifier_ref, np.ones(noise_map.num_terms)
+                "local_scales." + self._modifier_ref, np.ones(paulis.num_terms)
             )
-
-            if len(local_scale) != noise_map.num_terms:
-                raise SamplexRuntimeError(
-                    f"Received a noise map '{self._noise_ref}' with `{noise_map.num_terms}` "
-                    f"terms but a local scale from reference '{self._modifier_ref}' with "
-                    f"`{len(local_scale)}` terms."
-                )
-            scaled_rates = noise_map.rates * scale * local_scale
-            noise_map = PauliLindbladMap.from_sparse_list(
-                [
-                    (paulis, qubit_idxs, scaled_rate)
-                    for (paulis, qubit_idxs, _), scaled_rate in zip(
-                        noise_map.to_sparse_list(), scaled_rates
-                    )
-                ],
-                noise_map.num_qubits,
-            )
+            rates = rates * scale * local_scale
+        noise_map = PauliLindbladMap.from_sparse_list(
+            [(pauli, idxs, rate) for (pauli, idxs), rate in zip(paulis.to_sparse_list(), rates)],
+            num_qubits=paulis.num_qubits,
+        )
         signs, samples = noise_map.signed_sample(num_randomizations, rng.bit_generator.random_raw())
         registers[self._register_name] = PauliRegister(samples.to_dense_array().transpose())
         registers[self._sign_register_name] = Z2Register(signs.reshape(1, -1))

samplomatic/samplex/samplex_serialization.py
@@ -41,6 +41,7 @@ from .nodes.u2_param_multiplication_node import (
     LeftU2ParametricMultiplicationNode,
     RightU2ParametricMultiplicationNode,
 )
+from .noise_model_requirement import NoiseModelRequirement
 from .parameter_expression_table import ParameterExpressionTable
 from .samplex import Samplex
 
@@ -122,6 +123,21 @@ def _deserialize_passthrough_params(data: str) -> tuple[list[int], list[int]] | 
     return tuple(orjson.loads(data))
 
 
+def _serialize_noise_requirements(data: dict[str, NoiseModelRequirement]) -> str:
+    out_dict = {}
+    for name, spec in data.items():
+        out_dict[name] = orjson.dumps(spec._to_json_dict()).decode("utf-8")  # noqa: SLF001
+    return orjson.dumps(out_dict).decode("utf-8")
+
+
+def _deserialize_noise_requirements(data: str) -> dict[str, NoiseModelRequirement]:
+    outputs_raw = orjson.loads(data)
+    outputs = {}
+    for name, output in outputs_raw.items():
+        outputs[name] = NoiseModelRequirement._from_json(orjson.loads(output))  # noqa: SLF001
+    return outputs
+
+
 def _generate_graph_header(samplex: Samplex) -> dict[str, str]:
     return {
         "finalized": str(samplex._finalized),  # noqa: SLF001
@@ -129,6 +145,7 @@ def _generate_graph_header(samplex: Samplex) -> dict[str, str]:
         "input_specification": _serialize_specifications(samplex._input_specifications),  # noqa: SLF001
         "output_specification": _serialize_specifications(samplex._output_specifications),  # noqa: SLF001
         "passthrough_params": _serialize_passthrough_params(samplex._passthrough_params),  # noqa: SLF001
+        "noise_requirements": _serialize_noise_requirements(samplex._noise_model_requirements),  # noqa: SLF001
     }
 
 
@@ -139,13 +156,22 @@ def _process_graph_header(
     bool,
     dict[InterfaceName, TensorSpecification],
     dict[InterfaceName, TensorSpecification],
+    dict[str, NoiseModelRequirement],
 ]:
     raw_param_table_dict = orjson.loads(data["param_table"])
     param_table = _deserialize_expression_table(raw_param_table_dict)
     inputs = _deserialize_specifications(data["input_specification"])
     outputs = _deserialize_specifications(data["output_specification"])
     passthrough_params = _deserialize_passthrough_params(data["passthrough_params"])
-    return (param_table, data["finalized"] == "true", inputs, outputs, passthrough_params)
+    noise_requirements = _deserialize_noise_requirements(data["noise_requirements"])
+    return (
+        param_table,
+        data["finalized"] == "true",
+        inputs,
+        outputs,
+        passthrough_params,
+        noise_requirements,
+    )
 
 
 def samplex_to_json(samplex: Samplex, filename: str | None = None) -> str | None:
@@ -184,6 +210,7 @@ def _samplex_from_graph(samplex_graph: PyDiGraph) -> Samplex:
     samplex._input_specifications = graph_attrs[2]  # noqa: SLF001
     samplex._output_specifications = graph_attrs[3]  # noqa: SLF001
     samplex._passthrough_params = graph_attrs[4]  # noqa: SLF001
+    samplex._noise_model_requirements = graph_attrs[5]  # noqa: SLF001
     return samplex
 
 

samplomatic/tensor_interface.py
@@ -19,7 +19,7 @@ from enum import StrEnum
 from typing import Any, Literal, Self, overload
 
 import numpy as np
-from qiskit.quantum_info import PauliLindbladMap
+from qiskit.quantum_info import QubitSparsePauliList
 
 from .aliases import InterfaceName
 
@@ -29,7 +29,7 @@ class ValueType(StrEnum):
 
     BOOL = "bool"
     INT = "int"
-    LINDBLAD = "lindblad"
+    PAULIS = "paulis"
     NUMPY_ARRAY = "numpy_array"
 
 
@@ -78,7 +78,9 @@ class Specification:
     def validate_and_coerce(self: Literal[ValueType.INT], value: Any) -> int: ...
 
     @overload
-    def validate_and_coerce(self: Literal[ValueType.LINDBLAD], value: Any) -> PauliLindbladMap: ...
+    def validate_and_coerce(
+        self: Literal[ValueType.PAULIS], value: Any
+    ) -> QubitSparsePauliList: ...
 
     @overload
     def validate_and_coerce(self: Literal[ValueType.NUMPY_ARRAY], value: Any) -> np.ndarray: ...
@@ -99,8 +101,8 @@ class Specification:
             return bool(value)
         if self.value_type is ValueType.INT:
             return int(value)
-        if self.value_type is ValueType.LINDBLAD:
-            if isinstance(value, PauliLindbladMap):
+        if self.value_type is ValueType.PAULIS:
+            if isinstance(value, QubitSparsePauliList):
                 return value
         if self.value_type is ValueType.NUMPY_ARRAY:
             return np.array(value)

test/integration/test_inject_noise_samples.py
@@ -15,7 +15,7 @@
 import numpy as np
 from qiskit.circuit import BoxOp, QuantumCircuit
 from qiskit.circuit.library import CXGate
-from qiskit.quantum_info import Operator, Pauli, PauliLindbladMap
+from qiskit.quantum_info import Operator, Pauli, PauliLindbladMap, QubitSparsePauliList
 
 from samplomatic.annotations import InjectNoise, Twirl
 from samplomatic.builders import pre_build
@@ -29,10 +29,11 @@ def make_circuits():
     with circuit.box([Twirl(dressing="right")]):
         circuit.noop(0, 1)
 
-    noise_map = PauliLindbladMap.from_list([("II", 0.0)])
+    paulis = {"my_noise": QubitSparsePauliList.from_list(["II"])}
+    rates = {"rates.my_noise": [0.0]}
     expected = Operator(np.identity(16))
 
-    yield (circuit, expected, {"my_noise": noise_map}), "identity"
+    yield (circuit, expected, paulis, rates), "identity"
 
     circuit = QuantumCircuit(2)
     with circuit.box([Twirl(), InjectNoise("my_noise", "my_modifier")]):
@@ -41,13 +42,16 @@ def make_circuits():
     with circuit.box([Twirl(dressing="right")]):
         circuit.noop(0, 1)
 
-    yield (circuit, expected, {"my_noise": noise_map}), "identity_optional_modifiers"
+    yield (circuit, expected, paulis, rates), "identity_optional_modifiers"
+
+    paulis = {"my_noise": QubitSparsePauliList.from_list(["XX"])}
+    rates = {"rates.my_noise": [100.0]}
 
     noise_map = PauliLindbladMap.from_list([("XX", 100)])
     prob = next(iter(noise_map.probabilities()))
     expected = prob * Operator(np.identity(16)) + (1 - prob) * Operator(Pauli("XXXX").to_matrix())
 
-    yield (circuit, expected, {"my_noise": noise_map}), "xx_noise"
+    yield (circuit, expected, paulis, rates), "xx_noise"
 
     circuit = QuantumCircuit(2)
     with circuit.box([Twirl(), InjectNoise("my_noise")]):
@@ -59,7 +63,7 @@ def make_circuits():
     expected = prob * Operator(np.identity(16)) + (1 - prob) * Operator(Pauli("XXXX").to_matrix())
     expected = (Operator(CXGate()) ^ Operator(CXGate())) & expected
 
-    yield (circuit, expected, {"my_noise": noise_map}), "xx_noise_permuted"
+    yield (circuit, expected, paulis, rates), "xx_noise_permuted"
 
     circuit = QuantumCircuit(2)
     with circuit.box([Twirl(), InjectNoise("my_noise")]):
@@ -76,18 +80,22 @@ def make_circuits():
     expected = prob * Operator(np.identity(16)) + (1 - prob) * Operator(Pauli("XXXX").to_matrix())
     expected = expected & expected
 
-    yield (circuit, expected, {"my_noise": noise_map}), "xx_noise_twice"
+    yield (circuit, expected, paulis, rates), "xx_noise_twice"
+
+    paulis = {"my_noise": QubitSparsePauliList.from_list(["XI"])}
+    rates = {"rates.my_noise": [100.0]}
 
     noise_map = PauliLindbladMap.from_list([("XI", 100)])
     prob = next(iter(noise_map.probabilities()))
     expected = prob * Operator(np.identity(16)) + (1 - prob) * Operator(Pauli("XIXI").to_matrix())
+
     for idx, perm in enumerate([(0, 1), (1, 0)]):
         circuit = QuantumCircuit(2)
         with circuit.box([InjectNoise("my_noise"), Twirl()]):
             circuit.noop(*perm)
         with circuit.box([Twirl(dressing="right")]):
             circuit.noop(0, 1)
-        yield (circuit, expected, {"my_noise": noise_map}), f"permuted_context_qubits_{idx}"
+        yield (circuit, expected, paulis, rates), f"permuted_context_qubits_{idx}"
 
     noise_map = PauliLindbladMap.from_list([("XI", 100)])
     prob = next(iter(noise_map.probabilities()))
@@ -98,16 +106,16 @@ def make_circuits():
         circuit.append(box_op, perm)
         with circuit.box([Twirl(dressing="right")]):
             circuit.noop(0, 1)
-        yield (circuit, expected, {"my_noise": noise_map}), f"permuted_box_op_qubits_{idx}"
+        yield (circuit, expected, paulis, rates), f"permuted_box_op_qubits_{idx}"
 
 
 def pytest_generate_tests(metafunc):
     if "circuit" in metafunc.fixturenames:
         args, descriptions = zip(*make_circuits())
-        metafunc.parametrize("circuit,expected,noise_maps", list(args), ids=descriptions)
+        metafunc.parametrize("circuit,expected,paulis,rates", list(args), ids=descriptions)
 
 
-def test_sampling(circuit, expected, noise_maps, save_plot):
+def test_sampling(circuit, expected, paulis, rates, save_plot):
     """Test sampling.
 
     Casts the given ``circuit`` and the twirled circuit into operators, and it compares their
@@ -124,7 +132,7 @@ def test_sampling(circuit, expected, noise_maps, save_plot):
     save_plot(lambda: samplex_state.draw(), "Finalized Pre-Samplex", delayed=True)
     save_plot(lambda: samplex.draw(), "Samplex", delayed=True)
 
-    samplex_input = samplex.inputs().bind(noise_maps=noise_maps)
+    samplex_input = samplex.inputs(paulis).bind(noise_maps=rates)
     samplex_output = samplex.sample(samplex_input, num_randomizations=1000)
     parameter_values = samplex_output["parameter_values"]
 

commit 5ea004bdf879c22311d1ce70c7148f4be6086ccc

Fix `Samplex.__str__()` (#124)
## Summary

`Samplex.__str__()` was broken by #112 which caused `.inputs()` to
require arguments.

This PR also updates some interface descriptions, and fixes a bug in the
class
`NoiseModelRequirement`. However, this is an internal class and we did
not expose the bug,
so no need for a changelog on its account.

## Details and comments

---------

Co-authored-by: joshuasn <53916441+joshuasn@users.noreply.github.com>

samplomatic/samplex/noise_model_requirement.py
@@ -28,7 +28,7 @@ class NoiseModelRequirement:
     num_qubits: int
     """The number of qubits this model acts on."""
 
-    noise_modifiers: set = field(default=set)
+    noise_modifiers: set[str] = field(default_factory=set)
     """The set of modifiers that act on this noise model."""
 
     def _to_json_dict(self) -> dict[str, str]:

samplomatic/samplex/samplex.py
@@ -75,9 +75,14 @@ class Samplex:
         self._output_specifications: dict[InterfaceName, Specification] = {}
 
     def __str__(self):
+        noise_models = {
+            ref: QubitSparsePauliList.from_sparse_list([], num_qubits=requirement.num_qubits)
+            for ref, requirement in self._noise_model_requirements.items()
+        }
+        inputs = self.inputs(noise_models)
         return (
             f"Samplex(<{len(self.graph)} nodes>)\n"
-            f"  Inputs:\n{self.inputs().describe(prefix='    * ', width=100)}"
+            f"  Inputs:\n{inputs.describe(prefix='    * ', width=100)}"
             f"\n  Outputs:\n{self.outputs(123).describe(prefix='    * ', width=100)}".replace(
                 "[123", "[num_randomizations"
             )
@@ -271,7 +276,7 @@ class Samplex:
         noise_model_paulis = {} if noise_model_paulis is None else noise_model_paulis
         if noise_model_paulis.keys() != self._noise_model_requirements.keys():
             required_paulis = "\n".join(
-                f" * {ref}: '{req.num_qubits}' qubits."
+                f" * {ref}: A Pauli list on {req.num_qubits} qubits."
                 for ref, req in self._noise_model_requirements.items()
             )
             raise ValueError(
@@ -293,8 +298,9 @@ class Samplex:
                     f"noise_maps.rates.{name}",
                     (num_terms := (paulis.num_terms),),
                     np.dtype(np.float64),
-                    f"The rates of a noise map with ``{num_terms}`` terms acting on "
-                    f"``{noise_req.num_qubits}`` qubits.",
+                    f"The rates of a noise map with {num_terms} terms acting on "
+                    f"{noise_req.num_qubits} qubits. The order should match the order of the "
+                    "corresponding Pauli list.",
                 )
             )
 
@@ -313,7 +319,8 @@ class Samplex:
                         f"local_scales.{noise_modifier}",
                         (num_terms,),
                         np.dtype(np.float64),
-                        "An array of factors by which to scale individual rates of a noise map.",
+                        "An array of factors by which to scale individual rates of a noise map. "
+                        "The order should match the order of the corresponding Pauli list.",
                         optional=True,
                     )
                 )

test/unit/test_samplex/test_samplex.py
@@ -22,6 +22,7 @@ from qiskit.circuit import Parameter
 from samplomatic.exceptions import SamplexConstructionError, SamplexRuntimeError
 from samplomatic.optionals import HAS_PLOTLY
 from samplomatic.samplex import Samplex
+from samplomatic.samplex.noise_model_requirement import NoiseModelRequirement
 from samplomatic.samplex.samplex import wait_with_raise
 from samplomatic.tensor_interface import Specification, TensorSpecification, ValueType
 from samplomatic.virtual_registers import PauliRegister, U2Register
@@ -46,6 +47,7 @@ class TestBasic:
     def test_str(self):
         """Test the string dunder is doing fancy stuff."""
         samplex = Samplex()
+        samplex.add_noise_model_requirement(NoiseModelRequirement("ref", 5))
         assert "Samplex" in str(samplex)
         assert "Inputs:" in str(samplex)
         assert "Outputs:" in str(samplex)