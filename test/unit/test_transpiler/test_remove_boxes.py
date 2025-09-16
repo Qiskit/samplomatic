c854ef1c9b99768d60604da88eba15f0115a29da
Samuele Ferracin <sam.ferracin@ibm.com>
2025-09-16 08:58:35 -0400

    Update test/unit/test_transpiler/test_remove_boxes.py
    Co-authored-by: joshuasn <53916441+joshuasn@users.noreply.github.com>

--- test/unit/test_transpiler/test_remove_boxes.py
+++ test/unit/test_transpiler/test_remove_boxes.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Test the RemoveBoxes"""
+"""Test RemoveBoxes"""
 
 from qiskit.circuit import QuantumCircuit
 from qiskit.transpiler import PassManager
