bd0b2ea36074aa44e60f05922d03e55e836700a8
SamFerracin <sam.ferracin@ibm.com>
2025-10-27 16:38:07 -0400

    docs

--- samplomatic/samplex/samplex.py
+++ samplomatic/samplex/samplex.py
@@ -355,7 +355,7 @@ class Samplex:
 
             >>> # after binding required values, we can sample 123 randomizations
             >>> samplex.sample(
-            ...     inputs.bind(parameter_values=[0.1, 0.2]),
+            ...     {"parameter_values": [0.1, 0.2]},
             ...     num_randomizations=123,
             ... ) # doctest: +ELLIPSIS
             SamplexOutput({'measurement_flips.meas': ..., 'parameter_values': ...})
