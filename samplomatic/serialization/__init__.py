939f453f45d0519f18fba951df0b1eb28ac75e91
joshuasn <53916441+joshuasn@users.noreply.github.com>
2025-10-28 18:00:56 -0230

    Update samplomatic/serialization/__init__.py
    Co-authored-by: Ian Hincks <ian.hincks@gmail.com>

--- samplomatic/serialization/__init__.py
+++ samplomatic/serialization/__init__.py
@@ -10,4 +10,4 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Samplex Serialization Version"""
+"""Samplex Serialization"""
