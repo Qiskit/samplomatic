5c59c20a932846a8531ddfd20a2f329cd0531456
Ian Hincks <ian.hincks@gmail.com>
2025-12-11 16:09:59 -0500

    Update test/performance/test_transpiler.py
    Co-authored-by: joshuasn <53916441+joshuasn@users.noreply.github.com>

--- test/performance/test_transpiler.py
+++ test/performance/test_transpiler.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Test the transpiler method."""
+"""Test the transpiler."""
 
 import pytest
 from qiskit.transpiler import PassManager
