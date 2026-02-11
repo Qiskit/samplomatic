638bd1c9038d2f177936dad059242b62354dd669
Ian Hincks <ian.hincks@ibm.com>
2026-02-11 13:30:10 -0500

    fix

--- test/unit/test_distributions/test_balanced_uniform_pauli.py
+++ test/unit/test_distributions/test_balanced_uniform_pauli.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Test the PauliRegister distribution"""
+"""Test the BalancedUniformPauli distribution"""
 
 import numpy as np
 import pytest
