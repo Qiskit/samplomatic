22090a7f0086a70d2441ce5761d1b9bc4b5e2d25
Ian Hincks <ian.hincks@gmail.com>
2026-03-06 11:46:13 -0500

    Add balanced pauli sampling as a `Twirl.group` option (#300)
    ## Summary
    WIP for now, but close.
    ## Details and comments

--- samplomatic/distributions/__init__.py
+++ samplomatic/distributions/__init__.py
@@ -12,9 +12,18 @@
 
 """Distributions"""
 
+from ..annotations import GroupMode as _GroupMode
+from ..utils import FrozenDict as _FrozenDict
 from .balanced_uniform_pauli import BalancedUniformPauli
 from .distribution import Distribution
 from .haar_u2 import HaarU2
 from .uniform_c1 import UniformC1
 from .uniform_local_c1 import UniformLocalC1
 from .uniform_pauli import UniformPauli
+
+GROUP_TO_DISTRIBUTION: dict[_GroupMode, type[Distribution]] = _FrozenDict(
+    {
+        _GroupMode.PAULI: UniformPauli,
+        _GroupMode.BALANCED: BalancedUniformPauli,
+    }
+)
