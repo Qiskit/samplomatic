409838fe0d35e8c7a64186fc6dd9e6677cff293b
SamFerracin <sam.ferracin@ibm.com>
2026-08-09 15:47:34 -0400

    Added QuantumProgram and related classes

---
+++ samplomatic/quantum_program/__init__.py
@@ -0,0 +1,37 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Quantum Programs.
+
+Overview
+--------
+
+A quantum program consists of a list of ordered elements, each of which contains a single
+circuit and an array of associated parameter values. Executing a quantum program will
+sample the outcome of each circuit for the specified number of ``shots`` for each set of
+circuit arguments provided.
+
+
+Classes
+^^^^^^^
+
+.. autosummary::
+    :toctree: ../stubs/
+    :nosignatures:
+
+    QuantumProgram
+    QuantumProgramItem
+    CircuitItem
+    SamplexItem
+"""
+
+from .quantum_program import CircuitItem, QuantumProgram, QuantumProgramItem, SamplexItem
