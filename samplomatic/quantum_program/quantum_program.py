7512e9bc5269a859f72cadc9af50ae2141478e87
SamFerracin <sam.ferracin@ibm.com>
2026-08-10 15:31:35 -0400

    Removed 'meas_level'

--- samplomatic/quantum_program/quantum_program.py
+++ samplomatic/quantum_program/quantum_program.py
@@ -16,7 +16,7 @@ from __future__ import annotations
 
 import abc
 import math
-from typing import TYPE_CHECKING, Any, Literal
+from typing import TYPE_CHECKING, Any
 
 import numpy as np
 from qiskit.circuit import QuantumCircuit
@@ -224,21 +224,7 @@ class QuantumProgram:
         shots: The number of shots for each circuit execution.
         items: Items that comprise the program.
         noise_maps: Noise maps to use with samplex items.
-        meas_level: The level at which to return all classical register measurement results. This
-            value sets the return type of all classical registers in all quantum program items and
-            determines whether the raw complex data from low-level measurement devices is
-            discriminated into bits or not. The supported values are
-
-                * "classified": Classical register data is returned as boolean arrays with the
-                    intrinsic shape ``(num_shots, creg_size)``.
-                * "kerneled": Classical register data is returned as a complex array with the
-                    intrinsic shape ``(num_shots, creg_size)``, where each entry represents an IQ
-                    data point (resulting from kerneling the measurement trace) in arbitrary units.
-                * "avg_kerneled": Classical register data is returned as a complex array with the
-                    intrinsic shape ``(creg_size,)``, where data is equivalent to "kerneled" except
-                    additionally averaged over shots.
-
-            passthrough_data: Arbitrary nested data passed through execution without modification.
+        passthrough_data: Arbitrary nested data passed through execution without modification.
     """
 
     def __init__(
@@ -246,13 +232,11 @@ class QuantumProgram:
         shots: int,
         items: Iterable[QuantumProgramItem] | None = None,
         noise_maps: dict[str, PauliLindbladMap] | None = None,
-        meas_level: Literal["classified", "kerneled", "avg_kerneled", "both"] = "classified",
         passthrough_data: DataTree | None = None,
     ):
         self.shots = shots
         self.items: list[QuantumProgramItem] = list(items or [])
         self.noise_maps = noise_maps or {}
-        self.meas_level = meas_level
         self.passthrough_data = passthrough_data
 
         # Semantic role indicating how execution results may be post-processed by runtime clients.
